e648feadf7a39d6a52e475fe15fcc6fde209e19c
Habis <devkotahancy@gmail.com>
2021-11-17 02:54:08 +0545

    block chain started chain validating/replacement starting

--- block.js
+++ block.js
@@ -1,3 +1,7 @@
+const { GENESIS_DATA } = require('./config');
+const cryptoHash = require('./crypto-hash');
+
+
 class Block{
      constructor({timestamp, lastHash, hash, data})
      {
@@ -5,6 +9,21 @@ class Block{
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+     }
+     static genesis(){
+        return new this(GENESIS_DATA);
+     }
+     static mineBlock({ lastBlock, data}){
+
+      const  timestamp = Date.now();
+      const lastHash= lastBlock.hash;
+
+        return new this({
+           timestamp,
+           lastHash,
+           data,
+           hash: cryptoHash(timestamp, lastHash, data)
+        });
      }
 }
 
--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const GENESIS_DATA = {
 timestamp: 1,
 lastHash: '----',
 hash: 'hash-one',
-data = []
+data : []
 };
 module.exports = {GENESIS_DATA};
