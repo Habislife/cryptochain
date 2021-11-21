2f602fae158bc2cbb59f7d4d6a0caa293639e9fc
Habis <devkotahancy@gmail.com>
2021-11-21 20:26:16 +0545

    mining difficulty

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
+const INTIAL_DIFFICULTY = 3;
+
 const GENESIS_DATA = {
 timestamp: 1,
 lastHash: '----',
 hash: 'hash-one',
+difficulty: INTIAL_DIFFICULTY,
+nonce: 0,
 data : []
 };
 module.exports = {GENESIS_DATA};
