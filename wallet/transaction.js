abdfbd8ebb43d019d437d732314c0423c3a4bdc2
Habis <devkotahancy@gmail.com>
2021-11-28 21:52:35 +0545

    update transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -23,6 +23,17 @@ class Transaction{
              signature: senderWallet.sign(outputMap)
         };
     }
+
+    update({ senderWallet, recipient, amount})
+    {
+        this.outputMap[recipient] = amount;
+
+        this.outputMap[senderWallet.publicKey] =
+        this.outputMap[senderWallet.publicKey] - amount;
+
+        this.input = this.createInput({ senderWallet, outputMap:this.outputMap});
+    }
+
      static validTransaction(transaction){
         const { input:{ address, amount, signature }, outputMap } = transaction;
         const outputTotal = Object.values(outputMap)
