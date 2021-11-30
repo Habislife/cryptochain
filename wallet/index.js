9e96080c26ade45e0a03cc6cd422ddccd4b59133
Habis <devkotahancy@gmail.com>
2021-11-30 13:49:05 +0545

    calculate the wallet balance

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,7 @@
 const Transaction = require('./transaction');
 const { STARTING_BALANCE } = require('../config');
 const { ec, cryptoHash } = require('../util');
+const Block = require('../blockchain/block');
 class Wallet{
     constructor(){
         this.balance = STARTING_BALANCE;
@@ -16,6 +17,23 @@ class Wallet{
             throw new Error('Amount exceeds balance');
         }
         return new Transaction({senderWallet: this, recipient, amount });
+    }
+
+    static calculateBalance({ chain, address}){
+        let outputsTotal = 0;
+        for(let i = 1; i<chain.length; i++)
+        {
+            const block = chain[i];
+            for(let transaction of block.data)
+            {
+                const addressOutput = transaction.outputMap[address];
+
+                if(addressOutput){
+                    outputsTotal = outputsTotal + addressOutput;
+                }
+            }
+    }
+        return STARTING_BALANCE + outputsTotal ;
     }
 };
 module.exports = Wallet;
