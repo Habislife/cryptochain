885d3e0f08a8947b289f8d425d47f4494b35c9cf
Habis <devkotahancy@gmail.com>
2021-11-30 18:11:11 +0545

    valid transaction chain

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -130,7 +130,18 @@ describe('Blockchain', ()=>{
                 });
             });
         });
-    });
+        describe('and the `validTransctions` flag is true', ()=>{
+            it('calls validTransactionData()',()=>{
+                const validTransactionDataMock = jest.fn();
+
+                blockchain.validTransactionData = validTransactionDataMock;
+
+                newChain.addBlock({ data: 'foo' });
+                blockchain.replaceChain(newChain.chain,true);
+                expect(validTransactionDataMock).toHaveBeenCalled();
+            });
+        });
+    }); 
     describe('validTransactionData',()=>{
         let transaction, rewardTransaction, wallet;
 
